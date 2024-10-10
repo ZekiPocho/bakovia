0d556e8a4151db4395f1e67c193d39d0dea13164
ZekiPocho <zekimaldonado@gmail.com>
2024-10-10 02:10:09 -0400

    a

--- src/verificar.php
+++ src/verificar.php
@@ -3,10 +3,10 @@
     $email =$_POST['email'];
     $codigo =$_POST['codigo'];
     $res = $conn->query("SELECT * FROM usuarios 
-        where correo='$email' 
-        and token='$codigo' 
+        where correo='zekimaldonado@gmail.com' 
+        and token='3436' 
         ");
-    if( mysqli_num_rows($res) >= 1){
+    if( mysqli_num_rows($res) > 0){
         header('Location:../public/valid.html');
     }else{
         echo "codigo invalido";

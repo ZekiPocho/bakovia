4d947dce8ad84772d565c3e3150782f4f5a0b607
ZekiPocho <zekimaldonado@gmail.com>
2024-10-09 23:40:03 -0400

    xd

--- src/mail_msg.php
+++ src/mail_msg.php
@@ -1,6 +1,6 @@
 <?php
 // Varios destinatarios
-$para  = 'aidan@example.com' . ', '; // atención a la coma
+$para  = 'Papercut@user.com' . ', '; // atención a la coma
 //$para .= 'wez@example.com';
 
 // título
@@ -14,6 +14,7 @@ $mensaje = '
 </head>
 <body>
   <p>lorem ipsum</p>
+  <h2></h2>
 </body>
 </html>
 ';
@@ -21,12 +22,12 @@ $mensaje = '
 // Para enviar un correo HTML, debe establecerse la cabecera Content-type
 $cabeceras  = 'MIME-Version: 1.0' . "\r\n";
 $cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
+/*
 // Cabeceras adicionales
 $cabeceras .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
 $cabeceras .= 'From: Recordatorio <cumples@example.com>' . "\r\n";
 $cabeceras .= 'Cc: birthdayarchive@example.com' . "\r\n";
-$cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+$cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";*/
 
 // Enviarlo
 mail($para, $título, $mensaje, $cabeceras);

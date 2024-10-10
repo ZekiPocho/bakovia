21f5af60f9c1767312fe615770fc84c01bf4c927
ZekiPocho <zekimaldonado@gmail.com>
2024-10-10 02:08:47 -0400

    a

--- src/verificar.php
+++ src/verificar.php
@@ -6,7 +6,7 @@
         where correo='$email' 
         and token='$codigo' 
         ");
-    if( $res ){
+    if( mysqli_num_rows($res) = 1){
         header('Location:../public/valid.html');
     }else{
         echo "codigo invalido";

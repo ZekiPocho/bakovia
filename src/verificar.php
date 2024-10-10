ece2ac4ba5c09869de1377187b6ff1e00d9a5be8
ZekiPocho <zekimaldonado@gmail.com>
2024-10-10 00:47:51 -0400

    asd

---
+++ src/verificar.php
@@ -0,0 +1,15 @@
+<?php
+    include "db.php";
+    $email =$_POST['email'];
+    $codigo =$_POST['codigo'];
+    $res = $conexion->query("select * from usuarios 
+        where correo='$email' 
+        and token='$codigo' 
+        ")or die($conn->error);
+    if( mysqli_num_rows($res) > 0 ){
+        $conexion->query("update usuarios set verificado = 'si' where correo = '$email' ");
+
+    }else{
+        echo "codigo invalido";
+    }
+?>

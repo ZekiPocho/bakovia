07adbe9d84a7cb9afac744b7bb4f1f9c1daa2824
ZekiPocho <zekimaldonado@gmail.com>
2024-10-09 23:53:57 -0400

    xdd

--- src/mail_msg.php
+++ src/mail_msg.php
@@ -22,8 +22,8 @@ $mensaje = '
 
 // Para enviar un correo HTML, debe establecerse la cabecera Content-type
 $headers =  'MIME-Version: 1.0' . "\r\n"; 
-$headers .= 'From: Your name <info@address.com>' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+$headers = 'From: Your name <info@address.com>' . "\r\n";
+$headers = 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
 /*
 // Cabeceras adicionales
 $cabeceras .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
@@ -31,5 +31,5 @@ $cabeceras .= 'From: Recordatorio <cumples@example.com>' . "\r\n";
 $cabeceras .= 'Cc: birthdayarchive@example.com' . "\r\n";
 $cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";*/
 
-mail($para, $título, $mensaje, $cabeceras);
+mail($para, $título, $mensaje, $headers);
 ?>

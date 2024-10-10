c55156bd3694b699c53090fd084f4f3c785716cc
ZekiPocho <zekimaldonado@gmail.com>
2024-10-10 02:12:04 -0400

    aaa

--- src/verificar.php
+++ src/verificar.php
@@ -3,8 +3,8 @@
     $email =$_POST['email'];
     $codigo =$_POST['codigo'];
     $res = $conn->query("SELECT * FROM usuarios 
-        where correo='$_POST['email']' 
-        and token='$_POST['codigo']' 
+        where correo='$email' 
+        and token='$codigo' 
         ");
     if( mysqli_num_rows($res) > 0){
         header('Location:../public/valid.html');

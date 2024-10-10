536dd3c0316dec82b37f13526617fab4484e0896
ZekiPocho <zekimaldonado@gmail.com>
2024-10-10 01:44:15 -0400

    CARAJOMIERDA

--- src/verificar.php
+++ src/verificar.php
@@ -4,9 +4,9 @@
     $codigo =$_POST['codigo'];
     $res = $conn->query("select * from usuarios 
         where correo='$email' 
-
+        and token='$codigo'
         ")or die($conn->error);
-    if( $res->num_rows > 0 ){
+    if( $conn->query($res) === TRUE ){
         $conn->query("update usuarios set verificado = 'si' where correo = '$email' ");
         header('Location: ../valid.html');
     }

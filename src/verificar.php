82ece1a500c05ae647b297e09d29280c1f8e9710
ZekiPocho <zekimaldonado@gmail.com>
2024-10-10 01:40:46 -0400

    aaa

--- src/verificar.php
+++ src/verificar.php
@@ -2,11 +2,12 @@
     include "db.php";
     $email =$_POST['email'];
     $codigo =$_POST['codigo'];
+    $que = $conn->query("select * from usuarios token");
     $res = $conn->query("select * from usuarios 
         where correo='$email' 
         and token='$codigo' 
         ")or die($conn->error);
-    echo "$res";
+    echo "$que";
     if( $res->num_rows > 0 ){
         $conn->query("update usuarios set verificado = 'si' where correo = '$email' ");
         header('Location: ../valid.html');

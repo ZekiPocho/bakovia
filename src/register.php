52e362071a200af86c5bdc939edca63a299145d3
Nikogab120 <nico.cabrejos.z.g@gmail.com>
2024-10-10 21:33:06 -0400

    dsa

--- src/register.php
+++ src/register.php
@@ -1,23 +1,23 @@
 <?php 
    include "./db.php";
-   $error_message = "";  // Variable para almacenar el mensaje de error
+   if( isset($_POST['username'] ) && isset($_POST['email']) && isset($_POST['clave'] ) 
+    && isset($_POST['clave2'] )){
 
-   if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['clave']) && isset($_POST['clave2'])) {
+        if($_POST['clave'] == $_POST['clave2'] ){
+            $name=$_POST['username'];
+            $email=$_POST['email'];
+            $pass=sha1($_POST['clave']);
+            include "./mail_msg.php";
+            if($enviado){
+                $conn->query("insert into usuarios (nombre_usuario, correo, contrasena, verificado , token) 
+                    values('$name','$email','$pass','no','$codigo')  ")or die($conn->error);
+                    header('Location: ../public/sent.html');
+            }else{
+                echo "Error al enviar el Email, intente nuevamente";
+            }
+        }else{
+            echo "ERROR";
+        }
+    }
 
-       if ($_POST['clave'] == $_POST['clave2']) {
-           $name = $_POST['username'];
-           $email = $_POST['email'];
-           $pass = sha1($_POST['clave']);
-           include "./mail_msg.php";
-           if ($enviado) {
-               $conn->query("INSERT INTO usuarios (nombre_usuario, correo, contrasena, verificado, token) 
-                             VALUES ('$name', '$email', '$pass', 'no', '$codigo')") or die($conn->error);
-               header('Location: ../public/sent.html');
-           } else {
-               $error_message = "Error al enviar el Email, intente nuevamente";
-           }
-       } else {
-           $error_message = "Las contraseñas no coinciden.";
-       }
-   }
 ?>

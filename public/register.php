d033d77bd202a3f2271d20d4b91b50f1e8996923
Nikogab120 <nico.cabrejos.z.g@gmail.com>
2024-10-11 00:06:43 -0400

    asd

--- public/register.php
+++ public/register.php
@@ -1,3 +1,30 @@
+<?php 
+include "db.php"; // Asegúrate de que no haya espacios antes de esta línea
+
+if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['clave']) && isset($_POST['clave2'])) {
+    if ($_POST['clave'] == $_POST['clave2']) {
+        $name = $_POST['username'];
+        $email = $_POST['email'];
+        $pass = sha1($_POST['clave']);
+        include "mail_msg.php"; // Incluir archivo para enviar el correo
+
+        if ($enviado) {
+            // Inserción en la base de datos si el correo fue enviado
+            $conn->query("INSERT INTO usuarios (nombre_usuario, correo, contrasena, verificado, token) 
+                          VALUES ('$name', '$email', '$pass', 'no', '$codigo')") 
+                          or die($conn->error);
+            
+            // Redirigir a la página de confirmación
+            header("Location: ./sent.html");
+            exit(); // Asegúrate de detener la ejecución después del header
+        } else {
+            echo "Error al enviar el Email, intente nuevamente";
+        }
+    } else {
+        echo "<div class='alert alert-danger'>Las contraseñas no coinciden</div>";
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html class="no-js" lang="zxx">
@@ -165,33 +192,7 @@ aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle 
 </header>
 <!--TERMINA HEADER Y NAVBAR PRO-->
 
-<?php 
-include "db.php"; // Asegúrate de que no haya espacios antes de esta línea
 
-if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['clave']) && isset($_POST['clave2'])) {
-    if ($_POST['clave'] == $_POST['clave2']) {
-        $name = $_POST['username'];
-        $email = $_POST['email'];
-        $pass = sha1($_POST['clave']);
-        include "mail_msg.php"; // Incluir archivo para enviar el correo
-
-        if ($enviado) {
-            // Inserción en la base de datos si el correo fue enviado
-            $conn->query("INSERT INTO usuarios (nombre_usuario, correo, contrasena, verificado, token) 
-                          VALUES ('$name', '$email', '$pass', 'no', '$codigo')") 
-                          or die($conn->error);
-            
-            // Redirigir a la página de confirmación
-            header("Location: ./sent.html");
-            exit(); // Asegúrate de detener la ejecución después del header
-        } else {
-            echo "Error al enviar el Email, intente nuevamente";
-        }
-    } else {
-        echo "<div class='alert alert-danger'>Las contraseñas no coinciden</div>";
-    }
-}
-?>
 
     <!-- Start Account Register Area -->
     <div class="account-login section">

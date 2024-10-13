f64536f4c4a9477d9394620ae1f310f9cd894268
Nikogab120 <nico.cabrejos.z.g@gmail.com>
2024-10-13 16:11:16 -0400

    asdasd

--- public/login.php
+++ public/login.php
@@ -1,20 +1,20 @@
 <?php
 
 session_start();
-include "./db.php";
+include "db.php";
 
 // Verificar si las cookies existen
 if (isset($_COOKIE['email']) && isset($_COOKIE['password'])) {
     $email = $_COOKIE['email'];
     $password = $_COOKIE['password'];
-    
+
     // Verificar si los datos de las cookies son válidos
     $res = $conn->query("SELECT * FROM usuarios 
         WHERE correo='$email' 
         AND contrasena='$password'  
         AND verificado='si'") or die($conn->error);
-    
-    if (mysqli_num_rows($res) > 0) {
+
+    if ($res && mysqli_num_rows($res) > 0) {
         // Si es válido, iniciar la sesión automáticamente
         $_SESSION['user'] = $email;
         header("Location:../public/index.html");
@@ -25,6 +25,7 @@ if (isset($_COOKIE['email']) && isset($_COOKIE['password'])) {
 $email = $_POST['email'];
 $password = sha1($_POST['clave']);
 
+$mensaje = ""; // Agregar punto y coma
 
 // Consulta SQL para verificar el correo y la contraseña
 $res = $conn->query("SELECT * FROM usuarios 
@@ -32,24 +33,25 @@ $res = $conn->query("SELECT * FROM usuarios
     AND contrasena='$password'  
     AND verificado='si'") or die($conn->error);
 
-if (mysqli_num_rows($res) > 0) {
+if ($res && mysqli_num_rows($res) > 0) {
     // Iniciar sesión
-    session_start();
     $_SESSION['user'] = $email;
-    
+
     // Verificar si la opción de 'remember me' está seleccionada
     if (isset($_POST['remember'])) {
         // Generar cookies con duración de 30 días
         setcookie('email', $email, time() + (86400 * 30), "/"); // 86400 = 1 día
         setcookie('password', $password, time() + (86400 * 30), "/");
     }
-    
+
     // Redirigir al usuario a la página principal
     header("Location:../public/index.html");
 } else {
-    echo "<div class = 'alert alert-danger'> login incorrecto </div>";
+    $mensaje = "<div class='alert alert-danger'>Login incorrecto</div>";
 }
+
 ?>
+
 <!DOCTYPE html>
 <html class="no-js" lang="zxx">
 <?php

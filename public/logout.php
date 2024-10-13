903309ca70307bedb28cd910909d8b58a27cf014
ZekiPocho <zekimaldonado@gmail.com>
2024-10-13 18:41:12 -0400

    putas cookies

--- public/logout.php
+++ public/logout.php
@@ -1,5 +1,19 @@
 <?php
 session_start();
+
+// Destruir la sesión
 session_destroy();
+
+// Borrar las cookies 'email' y 'password'
+if (isset($_COOKIE['email'])) {
+    setcookie('email', '', time() - 3600, '/'); // Borra la cookie 'email'
+}
+
+if (isset($_COOKIE['password'])) {
+    setcookie('password', '', time() - 3600, '/'); // Borra la cookie 'password'
+}
+
+// Redirigir al usuario
 header("Location: ../public/index.html");
+exit(); // Termina la ejecución del script
 ?>

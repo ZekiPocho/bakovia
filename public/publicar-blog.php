9d0b64322f6317ac1bcfaba33a73a9964784f0d5
Nikogab120 <nico.cabrejos.z.g@gmail.com>
2024-10-16 20:10:34 -0400

    asd

--- public/publicar-blog.php
+++ public/publicar-blog.php
@@ -149,26 +149,24 @@ aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle 
 
 <section class="hero-area">
     <div class="container-sm">
+        <h1>
+            CREAR POST
+        </h1>
         <form action="POST">
-        <h2>Crear una nueva publicación</h2>
-    <form action="upload_post.php" method="post" enctype="multipart/form-data">
-        <!-- Título del post -->
-        <textarea name="" id="">
-        <label for="titulo">Título:</label><br>
-        <input type="text" id="titulo" name="titulo" required><br><br>
-        </textarea>
-
-        <!-- Cuerpo del post (opcional) -->
-        <label for="contenido">Contenido (opcional):</label><br>
-        <textarea id="contenido" name="contenido" rows="5" cols="50"></textarea><br><br>
-
+            <div>
+            <textarea class="form-control form-control-custom" name="" id="" placeholder="Título"></textarea>
+            </div>
+            <div>
+            <textarea class="form-control form-control-cuerpo form-control-custom" name="" id="" placeholder="Cuerpo"></textarea>
+            </div>
+            <div>
+            <!-- Subir imagen -->
         <!-- Subir una o más imágenes -->
         <label for="imagenes">Subir imágenes:</label><br>
         <input type="file" id="imagenes" name="imagenes[]" accept="image/*" multiple><br><br>
-
-        <!-- Botón para enviar el formulario -->
-        <input type="submit" value="Publicar">
-    </form>
+            </div>
+        <div class="row">
+        </div>
         </form>
     </div>
     

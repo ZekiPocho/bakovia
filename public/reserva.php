c1ec117765aa39115e7a0cddfacda6ec16fbd46e
ZekiPocho <zekimaldonado@gmail.com>
2024-10-16 23:15:51 -0400

    uy

--- public/reserva.php
+++ public/reserva.php
@@ -254,10 +254,9 @@ aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle 
 
                                         for ($mesa = 1; $mesa <= 3; $mesa++) {
                                             // Lógica para verificar si la mesa está ocupada
-                                            $query_reserva = "SELECT * 
-                                                            FROM `reserva_mesa` 
-                                                            WHERE fecha = CURRENT_DATE() AND id_hora_inicio <= 1 AND id_hora_final >= 1;
-                                                            "; // Usar CURDATE() para la fecha actual
+                                            $query_reserva = "SELECT * FROM reserva_mesa 
+                                                                WHERE id_mesa = $mesa 
+                                                                AND fecha = CURRENT_DATE() AND id_hora_inicio <= $hora AND id_hora_final >= $hora;"; // Usar CURDATE() para la fecha actual
                                             
                                             $result_reserva = mysqli_query($conn, $query_reserva);
 

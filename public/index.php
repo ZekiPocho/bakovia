6a28e23c35c210ad7b96a5b4a949857d61e767ed
Nikogab120 <nico.cabrejos.z.g@gmail.com>
2024-10-18 00:18:50 -0400

    asd

--- public/index.php
+++ public/index.php
@@ -420,7 +420,7 @@ aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle 
                             <div class="single-blog">
                                 <div class="blog-img">
                                     <a href="blog-single-sidebar.php?id='.$id_publicacion.'"> <!-- Enlace con el ID de la publicación -->
-                                        <img src="'.$imagen.'" alt="#" style="width: 300px; height: 200px; object-fit: cover;">
+                                        <img src="'.$imagen.'" alt="#" style="width: 370px; height: 215px; object-fit: cover;">
                                     </a>
                                 </div>
                                 <div class="blog-content">

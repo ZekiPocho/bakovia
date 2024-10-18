574ce7438bc6d046c849fb29a94b63d434abe874
Nikogab120 <nico.cabrejos.z.g@gmail.com>
2024-10-18 00:02:01 -0400

    asd

--- public/blog-grid-sidebar.php
+++ public/blog-grid-sidebar.php
@@ -235,7 +235,7 @@ while ($row = $result->fetch_assoc()) {
         <div class="single-blog">
             <div class="blog-img">
                 <a href="blog-single-sidebar.php?id='.$id_publicacion.'"> <!-- Pasar el ID en la URL -->
-                    <img src="'.$imagen.'" alt="#" width="370" height="230">
+                    <img src="'.$imagen.'" alt="#" width="480" height="270">
                 </a>
             </div>
             <div class="blog-content">

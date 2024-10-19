e13d1203662c473458377799ba8bb9754819de66
ZekiPocho <zekimaldonado@gmail.com>
2024-10-18 23:45:36 -0400

    mecago

--- public/reserva.php
+++ public/reserva.php
@@ -288,7 +288,6 @@ aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle 
                         <!-- COLUMNA DERECHA -->
                         <div class="col-md-6 text-center">
                             <h3>SELECCIONA</h3>
-                            <?php echo $dia_actual;?>
                             <br>
                             <form method="POST" action="../src/crear_partida.php">
                                 <div class="form-group">

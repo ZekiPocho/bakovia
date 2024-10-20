0cc0ccd4c0f77bb5cbfacd526d12d3d6c792fbf5
ZekiPocho <zekimaldonado@gmail.com>
2024-10-19 22:45:54 -0400

    AA

--- public/join-match.php
+++ public/join-match.php
@@ -4,7 +4,7 @@ require_once "../src/validate_session.php";
 include '../public/db.php'; // Asegúrate de incluir tu archivo de conexión a la base de datos
 
 $id_partida = $_POST['id_partida']; // Asegúrate de que el ID sea pasado de manera segura
-$_SESSION['id_partida'] = = $_POST['id_partida'];
+$_SESSION['id_partida'] = $_POST['id_partida'];
 
 // Preparar la consulta
 $query = "SELECT * FROM partida WHERE id_partida = ?";

1ff3d77632f5518e1e6980c8e042a81f1228a4be
ZekiPocho <zekimaldonado@gmail.com>
2024-10-19 20:09:28 -0400

    aver

--- public/matches.php
+++ public/matches.php
@@ -327,7 +327,15 @@ $conn->close();
                                             <span><?php echo $row['nombre_usuario1']; ?></span>
                                         </div>
                                         <div class="col-2">
-                                            <h7>PARTIDA ABIERTA</h7>
+                                            <?php
+                                            if ($usuario_actual === $row['nombre_usuario1']) {
+                                                echo '<a href="panel_control.php?id_partida=' . $row['id_partida'] . '" class="btn btn-primary">
+                                                ADMIN
+                                                </a>';
+                                            } else {
+                                                echo '<h7>PARTIDA ABIERTA</h7>';
+                                            }
+                                            ?>
                                         </div>
                                         <div class="col-5">
                                             <?php
@@ -388,74 +396,7 @@ $conn->close();
     </div>
     
 
-    <!-- Start Footer Area -->
-    <footer class="footer">
-        <!-- Start Footer Middle -->
-        <div class="footer-middle">
-            <div class="container">
-                <div class="bottom-inner">
-                    <div class="row">
-                        
-                        <div class="col-lg-3 col-md-6 col-12">
-                            <!-- Single Widget -->
-                            <div class="single-footer f-contact">
-                                <h3>¿QUIÉNES SOMOS?</h3>
-                                <p class="phone">Phone: +1 (900) 33 169 7720</p>
-                                <ul>
-                                    <li><span>Monday-Friday: </span> 9.00 am - 8.00 pm</li>
-                                    <li><span>Saturday: </span> 10.00 am - 6.00 pm</li>
-                                </ul>
-                                <p class="mail">
-                                    <a href="mailto:support@shopgrids.com">support@shopgrids.com</a>
-                                </p>
-                            </div>
-                            <!-- End Single Widget -->
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-12">
-                            <!-- Single Widget -->
-                            <div class="single-footer f-link">
-                                <h3>CATEGORÍAS</h3>
-                                <ul>
-                                    <li><a href="javascript:void(0)">About Us</a></li>
-                                    <li><a href="javascript:void(0)">Contact Us</a></li>
-                                    <li><a href="javascript:void(0)">Downloads</a></li>
-                                    <li><a href="javascript:void(0)">Sitemap</a></li>
-                                    <li><a href="javascript:void(0)">FAQs Page</a></li>
-                                </ul>
-                            </div>
-                            <!-- End Single Widget -->
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-12">
-                            <!-- Single Widget -->
-                            <div class="single-footer f-link">
-                                <h3>INFORMACIÓN</h3>
-                                <ul>
-                                    <li><a href="javascript:void(0)">Computers & Accessories</a></li>
-                                    <li><a href="javascript:void(0)">Smartphones & Tablets</a></li>
-                                    <li><a href="javascript:void(0)">TV, Video & Audio</a></li>
-                                    <li><a href="javascript:void(0)">Cameras, Photo & Video</a></li>
-                                    <li><a href="javascript:void(0)">Headphones</a></li>
-                                </ul>
-                            </div>
-                            <!-- End Single Widget -->
-                            
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-12">
-                            <!--Single Widget-->
-                            <div class="footer-logo">
-                                    <a href="index.html">
-                                        <img src="assets/images/logo/mini.png" alt="#">
-                                    </a>
-                            </div>
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Footer Middle -->
-    </footer>
-    <!--/ End Footer Area -->
+    
     <!-- ========================= scroll-top ========================= -->
     <a href="#" class="scroll-top">
         <i class="lni lni-chevron-up"></i>

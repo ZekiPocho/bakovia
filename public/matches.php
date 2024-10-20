c3b285e9e844c2021104ee4a025512eb5433343f
ZekiPocho <zekimaldonado@gmail.com>
2024-10-19 23:12:00 -0400

    xdd

--- public/matches.php
+++ public/matches.php
@@ -365,7 +365,7 @@ if ($result->num_rows > 0) {
                         // Verificar si el usuario en la sesión tiene made como 1
                         if ($row['made_usuario_sesion'] == 1) {
                             if ($row['nombre_usuario2'] !== "N/A") {
-                                echo <html>
+                                echo "<html>
                                 <div class='row align-items-center'>
                                     <div class='col-7'>
                                     <span><?php echo $row['nombre_usuario2']; ?></span>
@@ -373,7 +373,7 @@ if ($result->num_rows > 0) {
                                     <div class='col-5'>
                                         <img src='https://via.placeholder.com/50x50' alt='Foto de perfil' class='img-fluid'>
                                     </div>
-                                </div></html>;
+                                </div></html>";
                             }else {
                                 echo '<form action="join-match.php" method="POST" style="display:inline;">
                                     <input type="hidden" name="id_partida" value="' . $row['id_partida'] . '">

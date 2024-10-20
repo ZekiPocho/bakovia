90c4a720affe811f69e4549ccb025e5006192967
Nikogab120 <nico.cabrejos.z.g@gmail.com>
2024-10-20 00:55:48 -0400

    asd

--- public/blog-grid-sidebar.php
+++ public/blog-grid-sidebar.php
@@ -228,10 +228,14 @@ register.php<div class="col-sm-auto"></div>
             <div class="navbar-area">
                 <div clas="row-align-items-center">
                     <div class="col-lg-6 col-md-6 col-12">
-                        <p>Ultimas Publicaciones</p>
+                        <ul>
+                            <li>
+                            <p>Ultimas Publicaciones</p>
                     </div>
-                    <div class="col-md-6 col-12" right>
+                    <div class="col-md-6 col-12" >
                     <a aria-label="Toggle navigation" href="publicar-blog.php">Crear Blog <i class="lni lni-plus"></i></a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>

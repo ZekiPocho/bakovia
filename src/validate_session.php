82f183aa10578f485ad096cc373db12858071919
ZekiPocho <zekimaldonado@gmail.com>
2024-10-20 16:17:46 -0400

    xd

--- src/validate_session.php
+++ src/validate_session.php
@@ -1,8 +1,13 @@
 <?php
-
-session_start();
-if(!isset($_SESSION['user'])){
-    header("Location: ../public/login.php");
+// Verificar si la sesión no ha sido iniciada previamente
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
 }
 
+// Aquí va el código de validación de sesión, como verificar si el usuario está autenticado
+if (!isset($_SESSION['id_usuario'])) {
+    // Redirigir a la página de inicio de sesión si no está autenticado
+    header("Location: login.php");
+    exit();
+}
 ?>

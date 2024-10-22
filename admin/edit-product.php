44d0dd821da9effab040a65191f93df52eb4a2d3
ZekiPocho <zekimaldonado@gmail.com>
2024-10-22 02:27:50 -0400

    asd

--- admin/edit-product.php
+++ admin/edit-product.php
@@ -217,11 +217,11 @@ $juegos = getAllGames($conn);
 
         <label for="tipo">Tipo:</label>
         <select name="tipo" required>
-            <option value="Miniaturas"><?= $producto['tipo'] == 'Miniaturas' ? 'selected' : '' ?>Miniaturas</option>
-            <option value="Pinturas"><?= $producto['tipo'] == 'Pinturas' ? 'selected' : '' ?>Pinturas</option>
-            <option value="Suplementos"><?= $producto['tipo'] == 'Suplementos' ? 'selected' : '' ?>Suplementos</option>
-            <option value="Servicio"><?= $producto['tipo'] == 'Servicio' ? 'selected' : '' ?>Servicio</option>
-            <option value="Otros"><?= $producto['tipo'] == 'Otros' ? 'selected' : '' ?>Otros</option>
+            <option value="Miniaturas"<?= $producto['tipo'] == 'Miniaturas' ? 'selected' : '' ?>>Miniaturas</option>
+            <option value="Pinturas"<?= $producto['tipo'] == 'Pinturas' ? 'selected' : '' ?>>Pinturas</option>
+            <option value="Suplementos"<?= $producto['tipo'] == 'Suplementos' ? 'selected' : '' ?>>Suplementos</option>
+            <option value="Servicio"<?= $producto['tipo'] == 'Servicio' ? 'selected' : '' ?>>Servicio</option>
+            <option value="Otros"<?= $producto['tipo'] == 'Otros' ? 'selected' : '' ?>>Otros</option>
         </select>
 
         <button type="submit" name="update_product">Actualizar Producto</button>

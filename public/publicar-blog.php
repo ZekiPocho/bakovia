19e8c56a8f2f31df36001b5f0d426f28fc020315
ZekiPocho <zekimaldonado@gmail.com>
2024-10-21 23:12:50 -0400

    aver

--- public/publicar-blog.php
+++ public/publicar-blog.php
@@ -232,8 +232,8 @@ aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle 
             
             <input class="form-control" type="file" id="imagenes" name="imagenes[]" accept="image/*" multiple onchange="previewImages()" required style="display: none;">
             
-            <div class="image-upload" id="image-upload" style="width: 900px; height: 600px; flex; align-items: center; justify-content: center; cursor: pointer;">
-                <span class="text-muted"><img src="https://via.placeholder.com/900x600" alt="Foto de perfil" class="img-fluid"></span>
+            <div class="image-upload" id="image-upload" style="width: 600px; height: 300px; flex; align-items: center; justify-content: center; cursor: pointer;">
+                <span class="text-muted"><img src="https://via.placeholder.com/900x600" class="img-fluid"></span>
             </div>
 
             <div id="vista-previa" class="mt-2" style="display: flex; flex-wrap: wrap;"></div>

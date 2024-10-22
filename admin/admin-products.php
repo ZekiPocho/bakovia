27f8181930898839895b2980606acc029d1fec71
ZekiPocho <zekimaldonado@gmail.com>
2024-10-21 22:25:04 -0400

    xd

--- admin/admin-products.php
+++ admin/admin-products.php
@@ -242,7 +242,7 @@ $juegos = getAllGames($conn);
                 <tr>
                     <td><?= $producto['id_producto'] ?></td>
                     <td><?= $producto['nombre_producto'] ?></td>
-                    <td><?= $producto['descripcion'] ?></td>
+                    <td maxlength="50";><?= $producto['descripcion'] ?></td>
                     <td><?= $producto['precio'] ?></td>
                     <td><?= $producto['stock'] ?></td>
                     <td>

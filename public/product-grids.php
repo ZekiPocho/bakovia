0d68a1f6ad64bc353e90e634a030d48ae225d626
Nikogab120 <nico.cabrejos.z.g@gmail.com>
2024-10-23 00:15:21 -0400

    asd

--- public/product-grids.php
+++ public/product-grids.php
@@ -87,16 +87,16 @@ aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle 
         </ul>
       </li>
   </ul>
-  <form class="d-flex">
+  <form class="d-flex" action="search.php" method="GET">
     <div class="navbar-search search-style-5">
-            <div class="navbar-search search-input">
-                    <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-            </div>
-                <div class="search-btn">
-                    <button><i class="lni lni-search-alt"></i></button>
-                </div>
+        <div class="navbar-search search-input">
+            <input class="form-control me-2" type="search" name="query" placeholder="Buscar" aria-label="Search" required>
         </div>
-    </form>
+        <div class="search-btn">
+            <button type="submit"><i class="lni lni-search-alt"></i></button>
+        </div>
+    </div>
+  </form>
     
 </div>
 <!-- carrito -->

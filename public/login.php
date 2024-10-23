ef57da00c095f1b93c9e39e5c461b82afb58620e
Nikogab120 <nico.cabrejos.z.g@gmail.com>
2024-10-23 14:09:49 -0400

    asd

--- public/login.php
+++ public/login.php
@@ -150,128 +150,71 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </div>
     <!-- /End Preloader -->
 
-<!--HEADER Y NAVBAR PRO-->
+<!-- HEADER Y NAVBAR PRO -->
 <header class="header navbar-area">
     <nav class="navbar navbar-expand-lg">
-<div class="container">
-<a class="navbar-brand" href="index.php">
-    <img src="assets/images/logo/mini.png" alt="Logo" width="5">
-</a>
-<button class="navbar-toggler mobile-menu-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="toggler-icon"></span>
-    <span class="toggler-icon"></span>
-    <span class="toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    <li class="nav-item">
-      <a aria-label="Toggle navigation" href="index.php">PRINCIPAL</a>
-    </li>
-    <li class="nav-item">
-        <a class="active" class="nav-link dropdown-toggle" href="product-grids.php" data-bs-toggle="collapse"
-            data-bs-target="#submenu-1-2" aria-controls="navbarSupportedContent"
-            aria-expanded="false" aria-label="Toggle navigation">TIENDA</a>
-        <ul class="sub-menu collapse" id="submenu-1-2">
-            <li class="nav-item"><a href="about-us.html">About Us</a></li>
-            <li class="nav-item"><a href="faq.html">Faq</a></li>
-            <li class="nav-item"><a href="login.php">Login</a></li>
-            <li class="nav-item"><a href="register.php">Register</a></li>
-            <li class="nav-item"><a href="mail-success.html">Mail Success</a></li>
-            <li class="nav-item"><a href="404.html">404 Error</a></li>
-        </ul>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link dropdown-toggle" href="javascript:void(0)" data-bs-toggle="collapse"
-        data-bs-target="#submenu-1-3" aria-controls="navbarSupportedContent"
-        aria-expanded="false" aria-label="Toggle navigation">CATEGORÍAS</a>
-        <ul class="sub-menu collapse" id="submenu-1-3">
-          <li class="nav-item"><a href="#">Action</a></li>
-          <li class="nav-item"><a href="#">Another action</a></li>
-          <li class="nav-item"><a href="#">XD</a></li>
-        </ul>
-      </li>
-  </ul>
-  <form class="d-flex">
-    <div class="navbar-search search-style-5">
-            <div class="navbar-search search-input">
-                    <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-            </div>
-                <div class="search-btn">
-                    <button><i class="lni lni-search-alt"></i></button>
-                </div>
-        </div>
-    </form>
-    
-</div>
-<!-- carrito -->
-    <div class="navbar-cart">
-        <div class="cart-items">
-            <a href="javascript:void(0)" class="main-btn">
-                <i class="lni lni-cart"></i>
-                <span class="total-items">0</span>
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="assets/images/logo/mini.png" alt="Logo" width="5">
             </a>
-            <!-- Shopping Item -->
-            <div class="shopping-item">
-                <div class="dropdown-cart-header">
-                    <span>2 Productos</span>
-                    <a href="cart.html">Ver Carrito</a>
-                </div>
-                <ul class="shopping-list">
-                    <li>
-                        <a href="javascript:void(0)" class="remove" title="Remove this item"><i
-                                class="lni lni-close"></i></a>
-                        <div class="cart-img-head">
-                            <a class="cart-img" href="product-details.html"><img
-                                    src="assets/images/header/cart-items/item1.jpg" alt="#"></a>
-                        </div>
-
-                        <div class="content">
-                            <h4><a href="product-details.html">
-                                    Apple Watch Series 6</a></h4>
-                            <p class="quantity">1x - <span class="amount">$99.00</span></p>
-                        </div>
+            <button class="navbar-toggler mobile-menu-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="toggler-icon"></span>
+                <span class="toggler-icon"></span>
+                <span class="toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="active" aria-label="Toggle navigation" href="index.php">PRINCIPAL</a>
                     </li>
-                    <li>
-                        <a href="javascript:void(0)" class="remove" title="Remove this item"><i
-                                class="lni lni-close"></i></a>
-                        <div class="cart-img-head">
-                            <a class="cart-img" href="product-details.html"><img
-                                    src="assets/images/header/cart-items/item2.jpg" alt="#"></a>
-                        </div>
-                        <div class="content">
-                            <h4><a href="product-details.html">Wi-Fi Smart Camera</a></h4>
-                            <p class="quantity">1x - <span class="amount">$35.00</span></p>
-                        </div>
+                    <li class="nav-item">
+                        <a class="active" aria-label="Toggle navigation" href="product-grids.php">TIENDA</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link dropdown-toggle" href="javascript:void(0)" data-bs-toggle="collapse"
+                            data-bs-target="#submenu-1-3" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">CATEGORÍAS</a>
+                        <ul class="sub-menu collapse" id="submenu-1-3">
+                            <li class="nav-item"><a href="matches.php">Partidas</a></li>
+                            <li class="nav-item"><a href="blog-grid-sidebar.php">Blogs</a></li>
+                            <li class="nav-item"><a href="about-us.php">¿Quienes Somos?</a></li>
+                            <li class="nav-item"><a href="faq.php">Preguntas Frecuentes</a></li>
+                        </ul>
                     </li>
                 </ul>
-                <div class="bottom">
-                    <div class="total">
-                        <span>Total</span>
-                        <span class="total-amount">$134.00</span>
-                    </div>
-                    <div class="button">
-                        <a href="checkout.html" class="btn animate">Checkout</a>
+                <form class="d-flex">
+    <div class="navbar-search search-style-5">
+        <div class="navbar-search search-input">
+            <input id="search-input" class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+        </div>
+        <div class="search-btn">
+            <button type="button"><i class="lni lni-search-alt"></i></button>
+                <!-- Aquí se muestra el dropdown de búsqueda -->
+    <div id="search-dropdown" class="search-dropdown" style="display: none;"></div>
+        </div>
+    </div>
+</form>
+            </div>
+            
+                </div>
+            </div>
+            <!-- PERFIL -->
+            <div class="col-sm-auto">
+                <div class="navbar-cart">
+                    <div class="cart-items">
+                        <a href="profile.php" class="main-btn">
+                            <i class="lni lni-user"></i>
+                        </a>
                     </div>
                 </div>
             </div>
-            <!--/ End Shopping Item -->
-        </div>
-    </div>
-<div class="col-sm-auto"></div>
-    <div class="navbar-cart">
-        <div class="cart-items">
-            <a href="profile.php" class="main-btn">
-                <i class="lni lni-user"></i>
-            </a>
-        </div>
     </div>
 </div>
-</div>
-</nav>
+        </div>
+    </nav>
 </header>
-<!--TERMINA HEADER Y NAVBAR PRO-->
-
+<!-- TERMINA HEADER Y NAVBAR PRO --> 
 
     <!-- Start Account Login Area -->
     <div class="account-login section">

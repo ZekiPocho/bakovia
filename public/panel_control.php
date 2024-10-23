b2b87fa56073220ae9bc72b3cdbcfd21e763db3c
ZekiPocho <zekimaldonado@gmail.com>
2024-10-23 11:01:42 -0400

    asdasd

--- public/panel_control.php
+++ public/panel_control.php
@@ -319,7 +319,9 @@ aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle 
                 <input type="hidden" name="id_partida" value="<?php echo htmlspecialchars($id_partida); ?>">
                 <div class="mb-3">
                     <label for="rondas" class="form-label">Ronda Nº:</label>
-                    <input type="number" name="rondas" class="form-control" value="0"> <!-- Cambia este valor al número de rondas actual -->
+                    <center>
+                    <input type="number" style="width: 50px;" name="rondas" class="form-control" value="0"> <!-- Cambia este valor al número de rondas actual -->
+                    </center>
                 </div>
                 <button type="submit" class="btn btn-primary" 
                     <?php echo ($nombre_jugador2 === 'N/A') ? 'disabled' : ''; ?>>

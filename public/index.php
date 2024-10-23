0766411fe113a49ac2c7757a72cbc56769db0a9f
Nikogab120 <nico.cabrejos.z.g@gmail.com>
2024-10-23 14:01:24 -0400

    asd

--- public/index.php
+++ public/index.php
@@ -105,16 +105,7 @@ include ('db.php');
                         <a class="active" aria-label="Toggle navigation" href="index.php">PRINCIPAL</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link dropdown-toggle" href="product-grids.php" data-bs-target="#submenu-1-2"
-                            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">TIENDA</a>
-                        <ul class="sub-menu collapse" id="submenu-1-2">
-                            <li class="nav-item"><a href="about-us.html">About Us</a></li>
-                            <li class="nav-item"><a href="faq.html">Faq</a></li>
-                            <li class="nav-item"><a href="login.php">Login</a></li>
-                            <li class="nav-item"><a href="register.php">Register</a></li>
-                            <li class="nav-item"><a href="mail-success.html">Mail Success</a></li>
-                            <li class="nav-item"><a href="404.html">404 Error</a></li>
-                        </ul>
+                        <a class="active" aria-label="Toggle navigation" href="product-grids.php">PRINCIPAL</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link dropdown-toggle" href="javascript:void(0)" data-bs-toggle="collapse"

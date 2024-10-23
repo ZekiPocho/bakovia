19ef6f7bf1df25af421c96dbd13f168e83d38e72
Nikogab120 <nico.cabrejos.z.g@gmail.com>
2024-10-23 01:10:15 -0400

    asd

--- public/index.php
+++ public/index.php
@@ -21,7 +21,43 @@ include ('db.php');
     <link rel="stylesheet" href="assets/css/tiny-slider.css" />
     <link rel="stylesheet" href="assets/css/glightbox.min.css" />
     <link rel="stylesheet" href="assets/css/main.css" />
+    
+    <style>
+        .search-dropdown {
+            position: absolute;
+            background-color: white;
+            border: 1px solid #ccc;
+            max-height: 300px; /* Ajusta según sea necesario */
+            overflow-y: auto;
+            width: 100%;
+            z-index: 1000;
+            padding: 10px;
+        }
 
+        .search-dropdown h5 {
+            margin: 0;
+            font-size: 16px;
+        }
+
+        .search-dropdown ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        .search-dropdown ul li {
+            padding: 5px 0;
+        }
+
+        .search-dropdown ul li a {
+            text-decoration: none;
+            color: #333; /* Ajusta según tu diseño */
+        }
+
+        .search-dropdown ul li a:hover {
+            color: #ff9800; /* Cambia el color al pasar el mouse */
+        }
+    </style>
 </head>
 
 <body>

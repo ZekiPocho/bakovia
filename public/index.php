a180888bf23408f43a0ea1d928cdee610e62288b
ZekiPocho <zekimaldonado@gmail.com>
2024-11-02 14:19:24 -0400

    asdasd

--- public/index.php
+++ public/index.php
@@ -403,8 +403,8 @@ $result = mysqli_query($conn, $query);
                             <!-- Single Widget -->
                             <div class="single-footer f-contact">
                                 <h3>¿QUIÉNES SOMOS?</h3>
-                                Bakovia nace del cariño y el querer enseñar, transmitir y divulgar el hobby que es Warhammer.
-                                Deja el mundo atrás, entra al Bunker y alejate de la maldad del mundo.
+                                Bakovia nace del cariño y las ganas de compartir el hobby que amamos: ¡Warhammer! 
+                                Deja el mundo atrás, entra al Bunker y aléjate de la maldad del mundo.
                                 
                             </div>
                             <!-- End Single Widget -->
@@ -412,13 +412,12 @@ $result = mysqli_query($conn, $query);
                         <div class="col-lg-3 col-md-6 col-12">
                             <!-- Single Widget -->
                             <div class="single-footer f-link">
-                                <h3>CATEGORÍAS</h3>
+                                <h3>PÁGINAS</h3>
                                 <ul>
-                                    <li><a href="javascript:void(0)">About Us</a></li>
-                                    <li><a href="javascript:void(0)">Contact Us</a></li>
-                                    <li><a href="javascript:void(0)">Downloads</a></li>
-                                    <li><a href="javascript:void(0)">Sitemap</a></li>
-                                    <li><a href="javascript:void(0)">FAQs Page</a></li>
+                                    <li><a href="matches.php">Partidas</a></li>
+                                    <li><a href="blog-grid-sidebar.php">Publicaciones</a></li>
+                                    <li><a href="about-us.php">¿Quiénes Somos?</a></li>
+                                    <li><a href="faq.php">Preguntas Frecuentes</a></li>
                                 </ul>
                             </div>
                             <!-- End Single Widget -->
@@ -427,13 +426,9 @@ $result = mysqli_query($conn, $query);
                             <!-- Single Widget -->
                             <div class="single-footer f-link">
                                 <h3>INFORMACIÓN</h3>
-                                <ul>
-                                    <li><a href="javascript:void(0)">Computers & Accessories</a></li>
-                                    <li><a href="javascript:void(0)">Smartphones & Tablets</a></li>
-                                    <li><a href="javascript:void(0)">TV, Video & Audio</a></li>
-                                    <li><a href="javascript:void(0)">Cameras, Photo & Video</a></li>
-                                    <li><a href="javascript:void(0)">Headphones</a></li>
-                                </ul>
+                                Nos encontramos en la avenida Arce, edificio Santa Isabel Nº 2529.
+                                Atendemos de lunes a viernes, desde las 16:00 hasta las 21:00,
+                                 y los sábados de 13:00 a 18:00
                             </div>
                             <!-- End Single Widget -->
                             
